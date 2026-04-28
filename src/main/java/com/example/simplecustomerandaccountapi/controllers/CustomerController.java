849be7ae65d9c11e0bc2a2dba37b154b43597169
ColeS <colerobertosmith@gmail.com>
2026-04-28 12:34:10 -0400

    Created a get all customers endpoint and fixed a bean creation error

--- src/main/java/com/example/simplecustomerandaccountapi/controllers/CustomerController.java
+++ src/main/java/com/example/simplecustomerandaccountapi/controllers/CustomerController.java
@@ -1,4 +1,28 @@
 package com.example.simplecustomerandaccountapi.controllers;
 
+import com.example.simplecustomerandaccountapi.dtos.CustomerDto;
+import com.example.simplecustomerandaccountapi.mappers.CustomerMapper;
+import com.example.simplecustomerandaccountapi.repositories.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/customers")
 public class CustomerController {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
+        return ResponseEntity.ok(customerRepository.findAll()
+                .stream()
+                .map(customerMapper::toCustomerDto)
+                .toList());
+    }
 }
