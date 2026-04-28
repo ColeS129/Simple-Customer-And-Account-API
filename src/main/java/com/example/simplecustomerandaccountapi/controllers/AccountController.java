6c99381c8ee1b0f5ba94b367314e7dd8381d305e
ColeS <colerobertosmith@gmail.com>
2026-04-28 13:53:56 -0400

    Created an endpoint to get all accounts

--- src/main/java/com/example/simplecustomerandaccountapi/controllers/AccountController.java
+++ src/main/java/com/example/simplecustomerandaccountapi/controllers/AccountController.java
@@ -1,4 +1,28 @@
 package com.example.simplecustomerandaccountapi.controllers;
 
+import com.example.simplecustomerandaccountapi.dtos.AccountDto;
+import com.example.simplecustomerandaccountapi.mappers.AccountMapper;
+import com.example.simplecustomerandaccountapi.repositories.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/accounts")
 public class AccountController {
+    private AccountMapper accountMapper;
+    private AccountRepository accountRepository;
+
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccounts() {
+        return ResponseEntity.ok(accountRepository.findAll()
+                .stream()
+                .map(accountMapper::toAccountDto)
+                .toList());
+    }
 }
--- src/main/java/com/example/simplecustomerandaccountapi/mappers/AccountMapper.java
+++ src/main/java/com/example/simplecustomerandaccountapi/mappers/AccountMapper.java
@@ -3,8 +3,10 @@ package com.example.simplecustomerandaccountapi.mappers;
 import com.example.simplecustomerandaccountapi.dtos.AccountDto;
 import com.example.simplecustomerandaccountapi.entities.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
+    @Mapping(source = "customer.id", target = "customerId")
     AccountDto toAccountDto(Account account);
 }
