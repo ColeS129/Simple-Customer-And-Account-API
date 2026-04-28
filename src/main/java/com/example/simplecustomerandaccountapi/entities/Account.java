b7bccc129b8cf04f3d2291cf0f86a8ecc0e5ef54
ColeS <colerobertosmith@gmail.com>
2026-04-28 11:20:06 -0400

    Set up an account and customer entity.

--- src/main/java/com/example/simplecustomerandaccountapi/entities/Account.java
+++ src/main/java/com/example/simplecustomerandaccountapi/entities/Account.java
@@ -1,4 +1,29 @@
 package com.example.simplecustomerandaccountapi.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "balance")
+    private Double balance;
+
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    @ToString.Exclude
+    private Customer customer;
 }
--- src/main/java/com/example/simplecustomerandaccountapi/entities/Customer.java
+++ src/main/java/com/example/simplecustomerandaccountapi/entities/Customer.java
@@ -1,4 +1,43 @@
 package com.example.simplecustomerandaccountapi.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "customer", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<Account> accounts =  new ArrayList<>();
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+        account.setCustomer(this);
+    }
+
+    public void removeAccount(Account account) {
+        accounts.remove(account);
+        account.setCustomer(null);
+    }
 }
