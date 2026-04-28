021d9ae5119623e420c6b22e1b0e0785117b7e28
ColeS <colerobertosmith@gmail.com>
2026-04-28 14:04:46 -0400

    Created an endpoint to get all accounts for a specific customer ID.

--- src/main/java/com/example/simplecustomerandaccountapi/controllers/AccountController.java
+++ src/main/java/com/example/simplecustomerandaccountapi/controllers/AccountController.java
@@ -1,11 +1,13 @@
 package com.example.simplecustomerandaccountapi.controllers;
 
 import com.example.simplecustomerandaccountapi.dtos.AccountDto;
+import com.example.simplecustomerandaccountapi.entities.Account;
 import com.example.simplecustomerandaccountapi.mappers.AccountMapper;
 import com.example.simplecustomerandaccountapi.repositories.AccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,18 @@ public class AccountController {
                 .stream()
                 .map(accountMapper::toAccountDto)
                 .toList());
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<List<AccountDto>> getAccountsByCustomer(@PathVariable Long customerId) {
+        List<Account> accounts = accountRepository.findByCustomerId(customerId);
+        if (!accounts.isEmpty()) {
+            return ResponseEntity.ok(accounts.stream()
+                    .map(accountMapper::toAccountDto)
+                    .toList());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/example/simplecustomerandaccountapi/repositories/AccountRepository.java
+++ src/main/java/com/example/simplecustomerandaccountapi/repositories/AccountRepository.java
@@ -3,5 +3,8 @@ package com.example.simplecustomerandaccountapi.repositories;
 import com.example.simplecustomerandaccountapi.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByCustomerId(Long customerId);
 }
