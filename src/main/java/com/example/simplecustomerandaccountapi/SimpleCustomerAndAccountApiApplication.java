3a0c11bfec03216e0bbf45f217427b03a1a99ce3
ColeS <colerobertosmith@gmail.com>
2026-04-27 21:52:24 -0400

    Initial commit

---
+++ src/main/java/com/example/simplecustomerandaccountapi/SimpleCustomerAndAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecustomerandaccountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCustomerAndAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCustomerAndAccountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplecustomerandaccountapi/SimpleCustomerAndAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecustomerandaccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCustomerAndAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
